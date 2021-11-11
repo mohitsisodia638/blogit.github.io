29d16cb0d13e0002cb82b5c54a107c66040f16d1
Mohit Sisodia <60215473+mohitsisodia638@users.noreply.github.com>
2021-11-11 09:40:11 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
-import Head from 'next/head';
+import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
-import { FeaturedPosts } from '../sections/index';
-
 
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Blog-It</title>
-        <link rel="icon" href="/favicon.ico" />
-        
-      </Head>
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -32,6 +27,7 @@ export default function Home({ posts }) {
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
-    props: { posts }
+    props: { posts },
   };
 }
+
